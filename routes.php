d66c1234806f258a0148607d26d733f7c654c334
Breno Almeida Nunes de Souza <kbreno08@gmail.com>
2022-10-18 09:24:25 -0300

    [Dia 02] Refatorando routes

--- routes.php
+++ routes.php
@@ -9,14 +9,8 @@ class Routes {
 $page = ($_GET['page']);
 $controller = new Controller();
 
-switch ($page) {
-    case 'register':
-        $controller->doRegister();
-        break;
-    case 'not_found':
-        $controller->doNotFound();
-        break;
-    default:
-        $controller->doLogin();
-        break;
-}
+match($page){
+    'register' => $controller->doRegister(),
+    'not_found' => $controller->doNotFound(),
+    default => $controller->doLogin()
+};
